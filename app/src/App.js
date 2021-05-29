5f35231472a758f6938f1bb6805ab625a191b24d
LaurenVreeken <lvreeken@uw.edu>
2021-05-29 15:53:27 -0700

    set up demo site, ready to import package

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <h1>Er-Demo-site</h1>
+      <div id="map">
+        <p>ER MAP</p>
+        </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
