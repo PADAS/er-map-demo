e1873b699c2735d8419471e32a8d356917c09adc
zphillips <zagephillips@gmail.com>
2021-06-03 15:54:42 -0700

    testing

--- app/src/App.js
+++ app/src/App.js
@@ -7,7 +7,6 @@ function App() {
       <h1>Er-Demo-site</h1>
       <div>
         {/* <p>ER MAP</p> */}
-        <iframe height="500" width="1200" title="ER MAP" src={`${process.env.PUBLIC_URL}/build/index.html`}></iframe>
       </div>
     </div>
     </>
